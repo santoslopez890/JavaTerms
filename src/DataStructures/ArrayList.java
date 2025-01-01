3d2bbdc8ad7f351495556fa18ff73e89df3560eb
Santos Herrera <95710813+santoslopez890@users.noreply.github.com>
2024-12-31 20:57:29 -0500

    Update ArrayList.java

--- src/DataStructures/ArrayList.java
+++ src/DataStructures/ArrayList.java
@@ -1,4 +1,5 @@
 package DataStructures;
 
 public class ArrayList {
+//a dynamic array, growable array, resizable array, dynamic table, mutable array, or array list is a random access, variable-size list data structure that allows elements to be added or removed. It is supplied with standard libraries in many modern mainstream programming languages. Dynamic arrays overcome a limit of static arrays, which have a fixed capacity that needs to be specified at allocation.
 }
