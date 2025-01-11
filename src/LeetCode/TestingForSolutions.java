9f51167b0dd2baf86eb4d614c17c2d7629bf9cbc
Santos Herrera <santoslopez890@gmail.com>
2025-01-10 21:13:58 -0500

    technical

--- src/LeetCode/TestingForSolutions.java
+++ src/LeetCode/TestingForSolutions.java
@@ -1,8 +1,13 @@
 package LeetCode;
 
+
+import java.util.ArrayList;
+
 public class TestingForSolutions {
     public static void main(String[] args) {
         int i=0;
+        ArrayList bob=new ArrayList();
+        bob.indexOf(i);
         System.out.println(i++);
         System.out.println(i);
     }
---
+++ src/Technical.java
@@ -0,0 +1,5 @@
+public class Technical {
+    public static void main(String[] args) {
+
+    }
+}
